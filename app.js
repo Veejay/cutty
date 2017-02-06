d5abeec217f9fd852c20ea293e107f5e1a481033
Bertrand Chardon <bertrand.chardon@gmail.com>
2017-02-06 09:04:45 +0100

    Comment other style for image source

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/resize/:name/:width-:height', (request, response) => {
   // Use a local file (might have been uploaded before)
   const readStream = fs.createReadStream(`./uploads/${request.params.name}`)
   // Use a remote file
-  const uri = 'https://b1304dd34a40bcce1277-44e68fdffc19088dedb2d91dd3f404ec.ssl.cf1.rackcdn.com/files/5841ac1eff39355f64d3498a/size_2_creer-son-site-avec-orson.png'
-  const readStream = fetch({uri})
+  // const uri = 'https://b1304dd34a40bcce1277-44e68fdffc19088dedb2d91dd3f404ec.ssl.cf1.rackcdn.com/files/5841ac1eff39355f64d3498a/size_2_creer-son-site-avec-orson.png'
+  // const readStream = fetch({uri})
   // Getting the parameters from the request
   const {width, height} = request.params
   // Parameters are being transmitted as strings, convert to numbers
