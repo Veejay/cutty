41bfcf165ef4b53d82f51cf3f625cc8d0ca72c32
Bertrand Chardon <bertrand.chardon@gmail.com>
2017-07-10 14:59:08 +0200

    Resizing images

--- app.js
+++ app.js
@@ -7,23 +7,21 @@ const { resize } = require('./modules/resize')
 
 const app = express()
 
-app.get('/resize/:name/:width-:height', (request, response) => {
+app.get('/resize/:name/:width', (request, response) => {
+  console.log('Received a request')
   // Use a local file (might have been uploaded before)
   const readStream = fs.createReadStream(`./uploads/${request.params.name}`)
   // Use a remote file
-  // const uri = 'https://b1304dd34a40bcce1277-44e68fdffc19088dedb2d91dd3f404ec.ssl.cf1.rackcdn.com/files/5841ac1eff39355f64d3498a/size_2_creer-son-site-avec-orson.png'
-  // const readStream = fetch({uri})
+  // const uri = 'https://www.wired.com/wp-content/uploads/2015/09/google-logo.jpg'
+  // const readStream = fetch({ uri })
   // Getting the parameters from the request
-  const {width, height} = request.params
   // Parameters are being transmitted as strings, convert to numbers
-  const dimensions = [width, height].map(dimension => {
-    return parseInt(dimension, 10)
-  })
+  const width = parseInt(request.params.width, 10)
   // Pipe the readStream to sharp, then pipe the result to the client
-  readStream.pipe(resize(...dimensions)).pipe(response)
+  readStream.pipe(resize())//// WHAT IS HAPPENING HERE
 })
 
 // Start the server
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('Listening on port 3000')
 })
--- modules/resize.js
+++ modules/resize.js
@@ -1,9 +1,33 @@
 const sharp = require('sharp')
+const fs = require('fs')
 
-const resize = (width, height) => {
-  console.log(`Resizing image to ${width}x${height}`)
+let waitForFinish = writableStream => {
+  return new Promise((resolve, reject) => {
+    writableStream.on('close', event => {
+      resolve(true)
+    })
+  })
+}
+
+const resize = () => {
+  // IMPLICIT ZEAL.JPG
   // Specifying the target width while preserving the aspect ratio
-  return sharp().resize(width)
+  let pipeline = sharp()
+  let sizes = [200, 400, 600, 800, 1200, 1920]
+  let promises = sizes.map(size => {
+    var s = fs.createWriteStream(`uploads/${size}.jpg`)
+    pipeline.clone().resize(size).pipe(s)
+    return waitForFinish(s)
+  })
+  Promise.all(promises).then(done => {
+    console.log('Done')
+  }).catch(console.error)
+  // pipeline.clone().resize(1400).pipe()
+  // pipeline.clone().resize(400).pipe(fs.createWriteStream('uploads/400.jpg'))
+  // pipeline.clone().resize(200).pipe(fs.createWriteStream('uploads/200.jpg'))
+  // pipeline.clone().resize(720).pipe(fs.createWriteStream('uploads/720.jpg'))
+  // pipeline.clone().resize(600).pipe(fs.createWriteStream('uploads/600.jpg'))
+  // pipeline.clone().resize(1920).pipe(fs.createWriteStream('uploads/1920.jpg'))
 }
 
 module.exports = { resize }
